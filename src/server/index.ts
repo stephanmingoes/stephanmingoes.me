2291dfb2672f5f376852e5f824dcb8acaca93fee
stephanminges <stephanminges@gmail.com>
2023-11-11 18:30:33 -0500

    fix

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import { ContactFormSchema } from "@/lib/types";
 import { publicProcedure, router } from "./trpc";
 import { resend } from "./resend";
+import ContactMeEmailTemplate from "@/components/template/contactMeEmailTemplate";
 
 export const appRouter = router({
   contactMe: publicProcedure
@@ -10,19 +11,7 @@ export const appRouter = router({
         from: "Stephan <stephan@stephanmingoes.me>",
         to: [input.email],
         subject: `Thanks for reaching out`,
-        html: `    <div className="bg-white p-8 shadow-md rounded-lg">
-      <p className=" mb-2">Hello {name},</p>
-      <p className="text-muted-foreground mb-4">
-        Thank you for getting in touch. I&apos;m excited to hear from you!
-      </p>
-      <p className="text-muted-foreground mb-4">
-        Please feel free to reach out to me with again with any questions or
-        inquiries you may have. I&apos;ll do my best to get back to you as soon
-        as possible.
-      </p>
-      <p className="text-muted-foreground mb-4">Best regards,</p>
-      <p className="text-muted-foreground">Stephan Mingoes</p>
-    </div>`,
+        react: ContactMeEmailTemplate({ name: input.name }),
       });
       await resend.emails.send({
         from: "Stephan <stephan@stephanmingoes.me>",
