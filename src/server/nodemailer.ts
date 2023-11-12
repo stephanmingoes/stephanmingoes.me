6d706dc30644df1b3a33b5c29720dd124dd70df2
stephanminges <stephanminges@gmail.com>
2023-11-11 19:07:59 -0500

    fix

---
+++ src/server/nodemailer.ts
@@ -0,0 +1,10 @@
+import nodemailer from "nodemailer";
+export const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
+});
